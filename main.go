42c530b5e4b50dcbda1b1288644233e82f92f9a9
tinyravendev <119297118+tinyravendev@users.noreply.github.com>
2022-12-06 17:06:13 +0600

    Create main.go

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+)
+
+// Learning Structs, interfaces n stuff :(
+
+type CityState struct {
+	name     string
+	garrison map[string]int
+}
+
+func (ci CityState) Details() {
+
+	fmt.Printf("Name: %v\n", ci.name)
+	for key, value := range ci.garrison {
+		fmt.Printf("Garrison: %v %v\n", value, key)
+	}
+}
+
+func main() {
+
+	fmt.Println("\nCityState Simulator ver 1.0\n")
+
+	//check for existing config file
+
+	fmt.Println("TEMP:ERROR:No Save File Exists")
+	errors.New("ERR NOT IMPLEMENTED")
+
+	//Implement ui menu from lipgloss
+	fmt.Println("TEMP:STARTING NEW GAME")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("New City State Name:")
+	scanner.Scan()
+	ucname := scanner.Text()
+
+	PlayerState := CityState{
+		name:     ucname,
+		garrison: map[string]int{"Milita": 1000, "Infantry Swordsman": 500}}
+
+	PlayerState.Details()
+	// Implement Other functions/methods for invade(),develop(),tax(),hire()
+
+	panic("Nothing Else Is Implemented,Dinosaurs Attack Everyone \nThe End ")
+}
